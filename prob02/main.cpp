e9f7a733c86ef9f8c9f649410868efe0bc6fdc96
willyr2501 <willyr2501@cs104u-3a.acad.fullerton.edu>
2019-09-21 13:50:25 -0700

    Finished

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -6,21 +6,22 @@
 
 int main()
 {
-  std:;string first_name;
+  std::string first_name;
     std::cout << "Enter your first name: ";
   getline(std::cin, first_name);
 
   char middle_initial, last_initial;
   std::cout << "Enter the first initial of your middle name: ";
   std::cin.get(middle_initial);
+  std::cin.ignore();
 
 std::cout << "Enter the first initial of your last name: ";
   last_initial = std::cin.get();
   std::cin.ignore();
 
   // create username out of user's first name and middle/last initials
-  std::string 2username = first_name+middle_initial+last_initial;
-  std::cout >> "Your user name is \"" << username << ""\n";
+  std::string username = first_name+middle_initial+last_initial;
+  std::cout << "Your user name is \"" << username << "\"\n";
 
   return 0;
 }
