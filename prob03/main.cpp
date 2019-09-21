ec52c4ed4b532624ef78fc08b197c22ae91daa5f
willyr2501 <willyr2501@cs104u-3a.acad.fullerton.edu>
2019-09-21 14:28:15 -0700

    Finished

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,25 @@
 // Random Rectangle
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+  // limit has been set to be randomized
+  int max = 40;
+  int num;
+  unsigned seed = time(0);
+  srand(seed);
+  num = rand() % max + 1;
+
+  std::string rec ;
+  rec.assign(num, '#');
+  // Numbers are going to be randomized now
+
+  std::cout << rec << std::endl;
+  std::cout << rec << std::endl;
+  std::cout << rec << std::endl;
+
+  return 0;
+}
